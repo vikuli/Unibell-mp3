00ebff2bf0334af19d9794c5af3cfd7a20737ce9
vikuli <vikukulichka.terekhova@gmail.com>
2024-03-28 10:23:44 +0300

    fix paths

--- sound-list/src/app/data/audio-data.ts
+++ sound-list/src/app/data/audio-data.ts
@@ -5,24 +5,24 @@ export const AUDIO_DATA: Audio[] = [
     ID: 1,
     'Audio name': 'XOLIDAYBOY - Пожары',
     'Audio file name': 'XOLIDAYBOY - Пожары.mp3',
-    audioSrc: '../../assets/audio/XOLIDAYBOY - Пожары.mp3',
+    audioSrc: './assets/audio/XOLIDAYBOY - Пожары.mp3',
   },
   {
     ID: 2,
     'Audio name': 'Би-2 - Bowie',
     'Audio file name': 'Би-2 - Bowie.mp3',
-    audioSrc: '../../assets/audio/Би-2 - Bowie.mp3',
+    audioSrc: './assets/audio/Би-2 - Bowie.mp3',
   },
   {
     ID: 3,
     'Audio name': 'Порнофильмы - Ритуалы',
     'Audio file name': 'Порнофильмы - Ритуалы.mp3',
-    audioSrc: '../../assets/audio/Порнофильмы - Ритуалы.mp3',
+    audioSrc: './assets/audio/Порнофильмы - Ритуалы.mp3',
   },
   {
     ID: 4,
     'Audio name': 'Слот - Круги На Воде',
     'Audio file name': 'Слот - Круги На Воде.mp3',
-    audioSrc: '../../assets/audio/Слот - Круги На Воде.mp3',
+    audioSrc: './assets/audio/Слот - Круги На Воде.mp3',
   },
 ];
